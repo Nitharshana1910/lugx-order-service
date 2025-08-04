f2bdb9f6b80bc27b837069161e9948a34399faa8
Nitharshana <nitharsai@h3dynamics.com>
2025-08-04 16:49:49 +0530

    db test

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
+  host: process.env.DB_HOST || 'postgress',
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: 'lugx-gaming',
